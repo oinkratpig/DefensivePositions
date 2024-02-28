6bc4cc0247de81852ae76bc548388e7e2efbfef1
Oinky <contact.knilax@gmail.com>
2024-02-28 02:03:52 -0600

    Initial commit

---
+++ Harmony_Patch.cs
@@ -0,0 +1,96 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace DefensivePositions
+{
+    public class Harmony_Patch
+    {
+        private static Dictionary<AgentModel, PassageObjectModel> _returnPositions;
+        private static Dictionary<AgentModel, PassageObjectModel> _defensePositions;
+
+        // Constructor
+        public Harmony_Patch()
+        {
+            Oink.Log("Loading...");
+
+            HarmonyInstance harmony = HarmonyInstance.Create("oinkratpig.LobotomyCorp.DefensivePositions");
+            try
+            {
+                MethodInfo method = typeof(Harmony_Patch).GetMethod("UnitMouseEventManagerPatch");
+                harmony.Patch(typeof(UnitMouseEventManager).GetMethod("Update", AccessTools.all), null, new HarmonyMethod(method), null);
+            }
+            catch(Exception e)
+            {
+                Oink.Log(e.Message);
+            }
+
+            Oink.Log("Finished patches.");
+
+        } // end constructor
+
+        // Input
+        public static void UnitMouseEventManagerPatch(UnitMouseEventManager __instance)
+        {
+            // Return
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (Input.GetKey(KeyCode.LeftShift))
+                    _returnPositions = GetAllAgentPositions();
+                else
+                    SetAllAgentPositions(_returnPositions);
+            }
+            // Defense
+            else if (Input.GetKeyDown(KeyCode.Q))
+            {
+                if (Input.GetKey(KeyCode.LeftShift))
+                    _defensePositions = GetAllAgentPositions();
+                else
+                    SetAllAgentPositions(_defensePositions);
+            }
+
+        } // end UnitMouseEventManagerPatch
+
+        /// <summary>
+        /// Get the positions of every agent.
+        /// </summary>
+        private static Dictionary<AgentModel, PassageObjectModel> GetAllAgentPositions()
+        {
+            Dictionary<AgentModel, PassageObjectModel> dic = new Dictionary<AgentModel, PassageObjectModel>();
+            foreach(AgentUnit agent in AgentLayer.currentLayer.GetAgentList())
+            {
+                if (agent == null) continue;
+
+                AgentModel model = agent.model;
+                if (model == null) continue;
+                
+                dic.Add(model, Traverse.Create(model).Field("_currentWaitingPassage").GetValue<PassageObjectModel>());
+            }
+            return dic;
+
+        } // end GetAllAgentPositions
+
+        /// <summary>
+        /// Sets the positions of all agents.
+        /// </summary>
+        private static void SetAllAgentPositions(Dictionary<AgentModel, PassageObjectModel> positions)
+        {
+            foreach (AgentModel agent in positions.Keys)
+                if (agent != null)
+                {
+                    agent.SetWaitingPassage(positions[agent]);
+                    PassageObject passageObject = SefiraMapLayer.instance.GetPassageObject(positions[agent]);
+                    if (passageObject != null)
+                    {
+                        passageObject.OnPointEnter();
+                        passageObject.OnPointerClick();
+                    }
+                }
+            
+        } // end SetAllAgentPositions
+
+    } // end class
+
+} // end namespace
---
+++ Oink.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace DefensivePositions
+{
+	public static class Oink
+	{
+		private const bool DEBUG = false;
+
+		private static readonly string _logPath = @"C:\Users\knila\Desktop\Mods\Lobotomy Corp\DefensivePosition_Attempt2\log.txt";
+		private static int _logCount;
+
+		/// <summary>
+		/// Log message for debugging.
+		/// </summary>
+		public static void Log(string message)
+		{
+			#pragma warning disable CS0162 // Unreachable code detected
+			if (DEBUG)
+                using (StreamWriter writer = File.AppendText(_logPath))
+                    writer.WriteLine($"{DateTime.Now} {{{_logCount++}}} {message}");
+			#pragma warning restore CS0162 // Unreachable code detected
+
+		} // end Log
+
+	} // end Oink
+
+} // end namespace
